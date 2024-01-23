79b7c78c8019452e3ff45af2e52dab3ccbb388a0
Justus Kimutai <Justuskiprotich01@gmail.com>
2024-01-23 15:49:52 +0300

    finish task class and Project class

---
+++ main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("class Task{\n\n    constructor(title,description,duedate,prioritize,complete){\n        this.title = title\n        this.description = description\n        this.duedate = duedate\n        this.prioritize = prioritize\n        this.complete = complete\n    }\n\n    printTask(){\n        const task1 = {title,description,duedate,prioritize,complete};\n        console.log(task1);\n    }\n}\n\nconst task = new Task('Assignment','Soen 308','20th Feb',false,false)\n\ntask.printTask;\n\n//# sourceURL=webpack://todo/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
